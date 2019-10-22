ad4892e27983c468750c31ff69c5ad1c860833d5
milecofnas <milecofnas@gmail.com>
2019-10-22 21:17:23 +0200

    ejercicios

---
+++ operadores.js
@@ -0,0 +1,62 @@
+let numero1 = 4
+let n1 = " 4 + "
+let numero2 =  6
+let n2 = "6 "
+let texto = "sumo" 
+let texto2 = "y el resultado es "
+let resultado = numero1 + numero2
+console.log(texto + n1 + n2 + texto2 + resultado)
+
+let textob = "resto"
+let nb = " 4 - "
+let resultadob = numero1 - numero2
+console.log(textob + nb + n2 + texto2 + resultadob)
+
+let textoc = "multiplico"
+let nc = " 4 * "
+let resultadoc = numero1 * numero2
+console.log(textoc + nc + n2 + texto2 + resultadoc)
+
+let textod = "divido"
+let nd = " 4 / "
+let resultadod = numero1 / numero2
+console.log(textod + nd + n2 + texto2 + resultadod)
+
+let textoe = "hago el restro de"
+let ne = " 4 y "
+let resultadoe = numero1 % numero2
+console.log(textoe + ne + n2 + texto2 + resultadoe)
+
+let numero = 9
+console.log(numero * 1)
+console.log(numero * 2)
+console.log(numero * 3)
+console.log(numero * 4)
+console.log(numero * 5)
+console.log(numero * 6)
+console.log(numero * 7)
+console.log(numero * 8)
+console.log(numero * 9)
+console.log(numero * 10)
+
+let longitud = 10 
+let lados = 4 
+let perimetro = (longitud * lados)
+console.log(perimetro)
+
+let cuadrado = 5
+let perimetro2 = cuadrado ** 2
+console.log(perimetro2)
+
+let lado1 = 10
+let lado2 = 20
+let lado3 = 5 
+let perimetro3 = lado1 + lado2 + lado3 
+console.log(perimetro3)
+
+let altura = 10 
+let base = 4 
+let perimetro4 = altura + altura + base 
+let area = base * altura 
+console.log(perimetro4)
+console.log(area)
