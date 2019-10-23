11266521aa492e73e3b2353b202937a8f40571ea
milecofnas <milecofnas@gmail.com>
2019-10-23 13:37:19 +0200

    ++ejercicios

--- operadores.js
+++ operadores.js
@@ -67,4 +67,10 @@ let linea = "Cantidad de Personas: "
 console.log(linea + cantidadDePersonas)
 cantidadDePersonas -= 3
 console.log(linea + cantidadDePersonas)
-console.log(linea + cantidadDePersonas * 2)
+cantidadDePersonas += cantidadDePersonas
+console.log(linea + cantidadDePersonas)
+
+let numeroDeCliente = 143245
+let numeroDeClienteIngresado = "143245"
+console.log("El número ingresado por el cliente es correcto: ", numeroDeCliente == numeroDeClienteIngresado) 
+console.log("El cliente ingresó un número de cliente válido: ", numeroDeCliente === numeroDeClienteIngresado)
